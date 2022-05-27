88ed50bb7c8c901926d84e351a85a4c79111a899
itsrv23 <nifirus@gmail.com>
2022-05-27 14:30:59 +0300

    init

---
+++ src/test/java/ru/itsrv23/coursework2/Coursework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itsrv23.coursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
